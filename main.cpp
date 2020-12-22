66401048ac9440ccba0f3c620fe78a90248cbd73
rob <r.ferrio95@gmail.com>
2020-12-22 16:36:20 +0100

    fix modifica file: trunc allo start

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main() {
         std::vector<std::thread> threads;
         //Creo tanti thread quanti ne supporta la CPU concorrentemente, e in ognuno chiamo ioContext.run().
         //In questo modo ha un comportamento uguale a un thread pool: i completion handler sono invocati nel primo thread disponibile che ha chiamato la run()
-        for(int n = 0; n < std::thread::hardware_concurrency(); ++n){
+        for(int n = 0; n < std::thread::hardware_concurrency(); n++){
             threads.emplace_back([&]{ ioContext.run(); });
         }
         //Il thread principale si blocca al primo join
